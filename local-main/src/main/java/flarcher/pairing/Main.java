0814595316f16e7bcec9e5db5a6453db8b237b8a
Fabrice Larcher <fabrice.larcher@loansquare.com>
2020-02-11 21:16:18 +0100

    First version of web-app

--- local-main/src/main/java/flarcher/pairing/Main.java
+++ local-main/src/main/java/flarcher/pairing/Main.java
@@ -2,24 +2,46 @@ package flarcher.pairing;
 
 import com.google.common.base.Preconditions;
 import flarcher.pairing.http.PairingApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.awt.*;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		// Starts the API
-		new PairingApplication().run(args);
+	private static Logger LOGGER;
 
-		// Show the web-page
-		//Path currentPath = Paths.get(".").toAbsolutePath();
-		URL currentLocation = Main.class.getProtectionDomain().getCodeSource().getLocation();
-		Path currentPath = Paths.get(currentLocation.toURI()).getParent();
-		Preconditions.checkState(currentPath.toFile().isDirectory());
-		Desktop.getDesktop().open(currentPath.resolve("loading.html").toFile());
+	public static void main(String[] args) throws Exception {
+		Class<?> mainClass = MethodHandles.lookup().lookupClass();
+		LOGGER = LoggerFactory.getLogger(mainClass);
+
+		// Opens the web-app immediately
+		LOGGER.info("~~~~~ Web application Start ~~~~~~");
+		open(mainClass, "index.html");
+
+		// Starts the API
+		LOGGER.info("~~~~~ API Start ~~~~~~");
+		new PairingApplication().run(args);
 	}
 
+	private static void open(Class<?> clazz, String fileName) {
+		URL currentLocation = clazz.getProtectionDomain().getCodeSource().getLocation();
+		try {
+			Path currentPath = Paths.get(currentLocation.toURI()).getParent();
+			Preconditions.checkState(currentPath.toFile().isDirectory());
+			Desktop.getDesktop().open(currentPath.resolve(fileName).toFile());
+		}
+		catch (URISyntaxException e) {
+			LOGGER.error("Internal error");
+		}
+		catch (IOException e) {
+			LOGGER.error("Impossible to open " + fileName);
+		}
+	}
 }
