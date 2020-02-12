f22f3948cf0ae0d293bd17ce1ee0c728432d53d5
Fabrice Larcher <fabrice.larcher@loansquare.com>
2020-02-12 21:47:54 +0100

    First web app call to the API

--- web-app/src/main/webapp/pairing.js
+++ web-app/src/main/webapp/pairing.js
@@ -1,6 +1,29 @@
 
+// Calls a GET request to the API
+var callGet = (path, thenFn) => {
+	fetch(
+		'http://localhost:' + window.tpt.port.toFixed() + '/api/' + path)
+		.then((response) => {
+			if (!response.ok) {
+				console.error('GET call failed with status ' + response.status);
+			}
+			else {
+				response.json().then(thenFn);
+			}
+		});
+};
+
+var toStep = (index) => {
+	console.info('To step ' + index);
+	document.querySelectorAll('section:not([hidden])')
+		.forEach(s => s.setAttribute('hidden', ''));
+	document.getElementById('step' + index).removeAttribute('hidden');
+};
+
+// Application start
 window.onload = () => {
-	console.warn('Hello!');
+
+	toStep(1);
 
 	var port = 80;
 	var query = window.location.hash;
@@ -10,7 +33,11 @@ window.onload = () => {
 			port = parseInt(query.substring(begin + 1));
 		}
 	}
-
 	console.info('Port is ' + port);
+	window.tpt = { port: port };
+
+	callGet('tournament', json => {
+			toStep(2);
+		});
 };
 
