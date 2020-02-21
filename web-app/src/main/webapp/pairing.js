d60566d5be85af26d51c4add85c6feac86e9e96b
Fabrice Larcher <fabrice.larcher@loansquare.com>
2020-02-21 21:30:26 +0100

    Display an error if the browser is not supported

--- web-app/src/main/webapp/pairing.js
+++ web-app/src/main/webapp/pairing.js
@@ -1,10 +1,22 @@
 
+//-- Compatibility mode "ON" starting here
+
 var toStep = function(index) {
 	console.info('To step ' + index);
 	document.querySelectorAll('section.current')
 		.forEach(function(s) { s.classList.remove('current'); });
 	document.getElementById('step' + index).classList.add('current');
 };
+
+var unsupportedBrowser = false;
+window.addEventListener("load", function() {
+		if (!window.fetch) {
+			toStep(0); // Warning about non compatible browser
+			unsupportedBrowser = true;
+		}
+	}, true);
+
+//-- Compatibility mode "OFF" from here
 
 // Calls a GET request to the API
 var callGet = (path, thenFn) => {
@@ -21,7 +33,10 @@ var callGet = (path, thenFn) => {
 };
 
 // Application start
-window.onload = () => {
+window.addEventListener("load", () => {
+	if (unsupportedBrowser) {
+		return;
+	}
 
 	toStep(1);
 
@@ -39,5 +54,6 @@ window.onload = () => {
 	callGet('tournament', json => {
 			toStep(2);
 		});
-};
+
+}, true);
 
