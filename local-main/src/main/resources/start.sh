d18c8ad9d48e8955b9d2e8ba2e26dbe8c30be691
Fabrice Larcher <flarcher6@gmail.com>
2023-08-10 10:38:39 +0200

    Remove references to an older code

--- local-main/src/main/resources/start.sh
+++ local-main/src/main/resources/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 # Start script for Unix-based OS
-#
-# The first argument should be the path to the matrix input file
 
 LOCAL_DIR=$(dirname $0)
 java -jar "$LOCAL_DIR/${project.build.finalName}.jar" server "$LOCAL_DIR/configuration.yml" $1
